d1f4d9acf7c1d52aaa9b74a851bb4747810ee70b
Amy Jeanes <amyjeanes23@gmail.com>
2026-04-22 22:51:56 +0200

    UI: Add CAN Bridge toggle

--- openpilot/selfdrive/ui/mici/layouts/settings/developer.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/developer.py
@@ -1,4 +1,10 @@
+import os
+import signal
+import subprocess
 from collections.abc import Callable
+
+from openpilot.common.basedir import BASEDIR
+from openpilot.common.swaglog import cloudlog
 from openpilot.common.time_helpers import system_time_valid
 from openpilot.system.ui.widgets.scroller import NavScroller
 from openpilot.selfdrive.ui.mici.widgets.button import BigButton, BigToggle, BigParamControl, BigCircleParamControl, GreyBigButton
@@ -8,6 +14,8 @@ from openpilot.selfdrive.ui.layouts.settings.common import restart_needed_callba
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.selfdrive.ui.widgets.ssh_key import SshKeyFetcher
 
+BRIDGE_PATH = os.path.join(BASEDIR, "openpilot", "cereal", "messaging", "bridge")
+_bridge_proc: subprocess.Popen | None = None
 
 class AlphaLongConfirmPage(NavScroller):
   def __init__(self, on_confirm: Callable[[], None]):
@@ -73,6 +81,9 @@ class DeveloperLayoutMici(NavScroller):
     self._joystick_toggle = BigToggle("joystick debug mode",
                                       initial_state=ui_state.params.get_bool("JoystickDebugMode"),
                                       toggle_callback=self._on_joystick_debug_mode)
+    self._can_bridge_toggle = BigToggle("can bridge",
+                                      initial_state=_bridge_proc is not None and _bridge_proc.poll() is None,
+                                      toggle_callback=self._on_start_can_bridge)
     self._long_maneuver_toggle = BigToggle("longitudinal maneuver mode",
                                            initial_state=ui_state.params.get_bool("LongitudinalManeuverMode"),
                                            toggle_callback=self._on_long_maneuver_mode)
@@ -91,6 +102,7 @@ class DeveloperLayoutMici(NavScroller):
       self._ssh_toggle,
       self._ssh_keys_btn,
       self._joystick_toggle,
+      self._can_bridge_toggle,
       self._long_maneuver_toggle,
       self._lat_maneuver_toggle,
       self._alpha_long_toggle,
@@ -168,6 +180,21 @@ class DeveloperLayoutMici(NavScroller):
     self._long_maneuver_toggle.set_checked(False)
     ui_state.params.put_bool("LateralManeuverMode", False, block=True)
     self._lat_maneuver_toggle.set_checked(False)
+
+  def _on_start_can_bridge(self, state: bool):
+    global _bridge_proc
+    if state:
+      if _bridge_proc is None or _bridge_proc.poll() is not None:
+        try:
+          _bridge_proc = subprocess.Popen([BRIDGE_PATH, "can"])
+        except OSError:
+          cloudlog.exception("failed to start CAN bridge")
+          _bridge_proc = None
+          self._can_bridge_toggle.set_checked(False)
+    else:
+      if _bridge_proc is not None and _bridge_proc.poll() is None:
+        _bridge_proc.send_signal(signal.SIGTERM)
+        _bridge_proc = None
 
   def _on_long_maneuver_mode(self, state: bool):
     ui_state.params.put_bool("LongitudinalManeuverMode", state, block=True)
